94a7599d952112d3ef31f2a02c45ce67b791efbd
Viviane <106716101+profvivi@users.noreply.github.com>
2023-12-15 11:46:51 -0300

    s

--- js/main03.js
+++ js/main03.js
@@ -2,13 +2,10 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", () =>{
-    console.log("Cliquei no robô.")
+somar.addEventListener("click", (evento) =>{
+   braco.value = parseInt(braco.value ) 
 })
 
-function dizOi(nome) {
-    console.log("oi" + nome)
-    console.log("Bem-vindo ao Robotron 2024")
-}
-
-dizOi("Pedro");
+subtrair.addEventListener("click", (evento) =>{
+    braco.value = parseInt(braco.value ) 
+ })
