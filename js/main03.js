7f6fb9a5f7140edfaf3f65e94caae0daad4b9fae
Viviane <106716101+profvivi@users.noreply.github.com>
2023-12-15 11:32:43 -0300

    s

---
+++ js/main03.js
@@ -0,0 +1,10 @@
+robotron.addEventListener("click", () =>{
+    console.log("Cliquei no robô.")
+})
+
+function dizOi(nome) {
+    console.log("oi" + nome)
+    console.log("Bem-vindo ao Robotron 2024")
+}
+
+dizOi("Pedro");
